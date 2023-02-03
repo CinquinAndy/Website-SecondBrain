fd8fb7050347fd6b271bb7ac417a2ed59be9a02f
Cinquin Andy <andy.cinquin@infotel.com>
2023-02-03 11:30:39 +0100

    pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,143 +1,146 @@
 import clsx from 'clsx'
 
-import { Button } from '@/components/Button'
-import { CheckIcon } from '@/components/CheckIcon'
-import { Container } from '@/components/Container'
-import { GridPattern } from '@/components/GridPattern'
-import { SectionHeading } from '@/components/SectionHeading'
+import {Button} from '@/components/Button'
+import {CheckIcon} from '@/components/CheckIcon'
+import {Container} from '@/components/Container'
+import {GridPattern} from '@/components/GridPattern'
 
-function Plan({ name, description, price, features, href, featured }) {
-  return (
-    <div
-      className={clsx(
-        'relative px-4 py-16 sm:rounded-5xl sm:px-10 md:py-12 lg:px-12',
-        featured && 'bg-blue-600 sm:shadow-lg'
-      )}
-    >
-      {featured && (
-        <div className="absolute inset-0 text-white/10 [mask-image:linear-gradient(white,transparent)]">
-          <GridPattern x="50%" y="50%" />
-        </div>
-      )}
-      <div className="relative flex flex-col">
-        <h3
-          className={clsx(
-            'mt-7 text-lg font-semibold tracking-tight',
-            featured ? 'text-white' : 'text-slate-900'
-          )}
-        >
-          {name}
-        </h3>
-        <p
-          className={clsx(
-            'mt-2 text-lg tracking-tight',
-            featured ? 'text-white' : 'text-slate-600'
-          )}
-        >
-          {description}
-        </p>
-        <p className="order-first flex font-display font-bold">
-          <span
+function Plan({name, description, price, features, href, featured}) {
+    return (
+        <div
             className={clsx(
-              'text-[1.75rem] leading-tight',
-              featured ? 'text-blue-200' : 'text-slate-500'
+                'relative px-4 py-16 sm:rounded-5xl sm:px-10 md:py-12 lg:px-12',
+                featured && 'bg-indigo-900 sm:shadow-lg'
             )}
-          >
-            $
-          </span>
+        >
+            {featured && (
+                <div className="absolute inset-0 text-white/10 [mask-image:linear-gradient(white,transparent)]">
+                    <GridPattern x="50%" y="50%"/>
+                </div>
+            )}
+            <div className="relative flex flex-col">
+                <h3
+                    className={clsx(
+                        'mt-7 text-lg font-semibold tracking-tight',
+                        featured ? 'text-white' : 'text-slate-100'
+                    )}
+                >
+                    {name}
+                </h3>
+                <p
+                    className={clsx(
+                        'mt-2 text-lg tracking-tight',
+                        featured ? 'text-white' : 'text-slate-100'
+                    )}
+                >
+                    {description}
+                </p>
+                <p className="order-first flex font-display font-bold">
           <span
-            className={clsx(
-              'ml-1 mt-1 text-7xl tracking-tight',
-              featured ? 'text-white' : 'text-slate-900'
-            )}
+              className={clsx(
+                  'ml-1 mt-1 text-7xl tracking-tight',
+                  featured ? 'text-white' : 'text-slate-50'
+              )}
           >
             {price}
           </span>
-        </p>
-        <div className="order-last mt-8">
-          <ul
-            role="list"
-            className={clsx(
-              '-my-2 divide-y text-base tracking-tight',
-              featured
-                ? 'divide-white/10 text-white'
-                : 'divide-slate-200 text-slate-900'
-            )}
-          >
-            {features.map((feature) => (
-              <li key={feature} className="flex py-2">
-                <CheckIcon
-                  className={clsx(
-                    'h-8 w-8 flex-none',
-                    featured ? 'fill-white' : 'fill-slate-600'
-                  )}
-                />
-                <span className="ml-4">{feature}</span>
-              </li>
-            ))}
-          </ul>
+                    <span
+                        className={clsx(
+                            'text-[1.75rem] leading-tight',
+                            featured ? 'text-blue-200' : 'text-slate-300'
+                        )}
+                    >
+            {featured ? '€' : '€/mois'}
+          </span>
+                </p>
+                <div className="order-last mt-8">
+                    <ul
+                        role="list"
+                        className={clsx(
+                            '-my-2 divide-y text-base tracking-tight',
+                            featured
+                                ? 'divide-white/10 text-white'
+                                : 'divide-white/10 text-slate-50'
+                        )}
+                    >
+                        {features.map((feature) => (
+                            <li key={feature} className="flex py-2">
+                                <CheckIcon
+                                    className={clsx(
+                                        'h-8 w-8 flex-none',
+                                        featured ? 'fill-white' : 'fill-slate-50'
+                                    )}
+                                />
+                                <span className="ml-4">{feature}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <Button
+                    href={href}
+                    color={featured ? 'white' : 'slate'}
+                    className="mt-8"
+                    aria-label={`Get started with the ${name} plan for $${price}`}
+                >
+                    Ça m&apos;intéresse
+                </Button>
+            </div>
         </div>
-        <Button
-          href={href}
-          color={featured ? 'white' : 'slate'}
-          className="mt-8"
-          aria-label={`Get started with the ${name} plan for $${price}`}
-        >
-          Get started
-        </Button>
-      </div>
-    </div>
-  )
+    )
 }
 
 export function Pricing() {
-  return (
-    <section
-      id="pricing"
-      aria-labelledby="pricing-title"
-      className="scroll-mt-14 pt-16 pb-8 sm:scroll-mt-32 sm:pt-20 sm:pb-10 lg:pt-32 lg:pb-16"
-    >
-      <Container>
-        <SectionHeading number="4" id="pricing-title">
-          Pricing
-        </SectionHeading>
-        <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">
-          Pick your package
-        </p>
-        <p className="mt-4 max-w-xl text-lg tracking-tight text-slate-600">
-          “Everything Starts as a Square” is available in two different packages
-          so you can pick the one that’s right for you.
-        </p>
-      </Container>
-      <div className="mx-auto mt-16 max-w-5xl lg:px-6">
-        <div className="grid bg-slate-50 sm:px-6 sm:pb-16 md:grid-cols-2 md:rounded-6xl md:px-8 md:pt-16 lg:p-20">
-          <Plan
-            name="Essential"
-            description="The perfect starting point if you’re on a budget."
-            price={15}
-            href="#"
-            features={[
-              'The 240-page ebook',
-              'Figma icon templates',
-              'Community access',
-            ]}
-          />
-          <Plan
-            featured
-            name="Complete"
-            description="Everything icon resource you could ever ask for."
-            price={229}
-            href="#"
-            features={[
-              'The 240-page ebook',
-              'Figma icon templates',
-              'Over an hour of screencasts',
-              'Weekly icon teardowns',
-              'Community access',
-            ]}
-          />
-        </div>
-      </div>
-    </section>
-  )
+    return (
+        <section
+            id="pricing"
+            aria-labelledby="pricing-title"
+            className="scroll-mt-14 pt-16 pb-8 sm:scroll-mt-32 sm:pt-20 sm:pb-10 lg:pt-32 lg:pb-16"
+        >
+            <Container>
+                <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-slate-50 sm:text-6xl">
+                    Choisissez votre formule
+                </p>
+                <p className="mt-4 max-w-xl text-lg tracking-tight text-slate-200">
+                    Je vous propose 2 formules complètement différentes, pour vous permettre de choisir celle qui vous convient le mieux.
+                </p>
+            </Container>
+            <div className="mx-auto mt-16 max-w-5xl lg:px-6">
+                <div className="grid bg-white/5 rounded-2xl sm:px-6 sm:pb-16 md:grid-cols-2 md:rounded-6xl md:px-8 md:pt-16 lg:p-20">
+                    <Plan
+                        name="Accès à mon second cerveau"
+                        description="Parfait pour ceux qui veulent se former. Mais qui ne savent pas par où commencer. Débutant ou confirmé, vous trouverez votre bonheur."
+                        price={4.99}
+                        href="mailto:contact@andy-cinquin.fr"
+                        features={[
+                            'Technos back-end',
+                            'Technos front-end',
+                            'Technos cloud',
+                            'Technos IA',
+                            'Technos DevOps',
+                            'Technos UX/UI',
+                            'Design',
+                            'Marketing',
+                            'Gestion de projet',
+                        ]}
+                    />
+                    <Plan
+                        featured
+                        name="Formation complète"
+                        description="Vous voulez faire votre propre second cerveau ? Ce plan est fait pour vous. Je vous accompagne de A à Z
+                        pour créer cet outil fantastique !"
+                        price={299}
+                        href="mailto:contact@andy-cinquin.fr"
+                        features={[
+                            'Accompagnement personnalisé',
+                            'Réponse rapide à toutes vos questions',
+                            'Partage de bonnes pratiques',
+                            'Partage d\'expériences',
+                            'Atteinte des objectifs plus rapidement',
+                            'Plus grande efficacité dans votre organisation'
+                        ]}
+                    />
+                </div>
+            </div>
+        </section>
+    )
 }
