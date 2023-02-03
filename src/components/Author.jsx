26e67aa7f958dc60c1db29c3f358f07a24d06168
Cinquin Andy <andy.cinquin@infotel.com>
2023-02-03 12:19:19 +0100

    author and logo malt modified

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -20,15 +20,15 @@ export function Author() {
             aria-labelledby="author-title"
             className="relative scroll-mt-14 pb-3 pt-8 sm:scroll-mt-32 sm:pb-16 sm:pt-10 lg:pt-16"
         >
-            <div className="absolute inset-x-0 bottom-0 top-1/2 text-slate-900/10 [mask-image:linear-gradient(transparent,white)]">
+            <div className="absolute inset-x-0 bottom-0 top-1/2 text-slate-50/20 [mask-image:linear-gradient(transparent,white)]">
                 <GridPattern x="50%" y="100%"/>
             </div>
             <div className="relative mx-auto max-w-5xl pt-16 sm:px-6">
-                <div className="bg-slate-50 pt-px sm:rounded-6xl">
+                <div className="bg-slate-50 pt-px sm:rounded-2xl">
                     <div
-                        className="relative mx-auto -mt-16 h-44 w-44 overflow-hidden rounded-full bg-slate-200 md:float-right md:h-64 md:w-64 md:[shape-outside:circle(40%)] lg:mr-20 lg:h-72 lg:w-72">
+                        className="relative mx-auto -mt-24 h-44 w-44 overflow-hidden rounded-full shadow-2xl md:float-right md:h-64 md:w-64 md:[shape-outside:circle(40%)] lg:mr-10 lg:h-72 lg:w-72 bg-white/10">
                         <Image
-                            className="absolute inset-0 h-full w-full object-cover"
+                            className="absolute inset-0 h-full w-full object-cover transform scale-50"
                             src={authorImage}
                             alt=""
                             sizes="(min-width: 1024px) 18rem, (min-width: 768px) 16rem, 11rem"
@@ -45,15 +45,16 @@ export function Author() {
                             développeur passionné et entrepreneur depuis plusieurs années.
                         </p>
                         <p className="mt-4 text-lg tracking-tight text-slate-700">
-                            Je souhaite partager avec vous mes connaissance et mon expérience dans le domaine exceptionnel du développement.
+                            Je souhaite partager avec vous mes connaissances et mon expérience dans le domaine exceptionnel du développement.
+                            Et le monde de la tech&apos;.
                         </p>
                         <p className="mt-8">
                             <Link
-                                href="#"
+                                href="mailto:contact@andy-cinquin.fr"
                                 className="inline-flex items-center text-base font-medium tracking-tight text-blue-600"
                             >
                                 <TwitterIcon className="h-10 w-10 fill-current"/>
-                                <span className="ml-4">Follow on Twitter</span>
+                                <span className="ml-4"></span>
                             </Link>
                         </p>
                     </div>
