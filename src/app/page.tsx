e84c6d2985c35044c6b5e20b6586913135cb7ebc
Cinquin Andy <andy.cinquin@infotel.com>
2023-02-03 10:45:11 +0100

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import {Hero} from '@/components/Hero'
 import {Introduction} from '@/components/Introduction'
+import {Pricing} from '@/components/Pricing'
 
 export default function Home() {
     return (
         <main>
             <Hero/>
             <Introduction/>
+            <Pricing/>
         </main>
     )
 }
