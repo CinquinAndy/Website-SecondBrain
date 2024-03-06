7a13d4cb0c3f69550ceff804da1f6c634860057b
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-06 20:09:38 +0100

    ✨ Refactor layout structure and metadata handling
    Simplified RootLayout function by restructuring HTML elements, updating meta description content, and optimizing script loading for better performance.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,36 +4,30 @@ import {Footer} from '@/components/Footer'
 import {Header} from '@/components/Header'
 import Script from "next/script";
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
-    return (
-        <html lang="fr">
-        {/*
+export default function RootLayout({children}) {
+    return (<html lang="fr">
+    {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-        <head>
-            <title>Mon second cerveau</title>
-            <meta
-                name="description"
-                content="J'ai utilisé plusieurs techniques d'organisation de vie et de prises de notes pour gérer mon travail et mes projets.
+    <head>
+        <title>Mon second cerveau</title>
+        <meta
+            name="description"
+            content="J'ai utilisé plusieurs techniques d'organisation de vie et de prises de notes pour gérer mon travail et mes projets.
                 J'ai décidé de les rassembler dans un seul outil. Et de vous donner la possibilité d'y accéder !"
-            />
-        </head>
-        <Script
-            async
-            src="https://umami.wadefade.fr/script.js"
-            strategy={'afterInteractive'}
-            data-website-id="3fc369c1-7f05-47e4-baaa-85eb44808024"
         />
-        <body className={"bg-[#0b070f]"}>
-        <Header/>
-        {children}
-        <Footer/>
-        </body>
-        </html>
-    )
+    </head>
+    <Script
+        async
+        src="https://umami.wadefade.fr/script.js"
+        strategy={'afterInteractive'}
+        data-website-id="3fc369c1-7f05-47e4-baaa-85eb44808024"
+    />
+    <body className={"bg-[#0b070f]"}>
+    <Header/>
+    {children}
+    <Footer/>
+    </body>
+    </html>)
 }
